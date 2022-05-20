6a68756e1c636d9a9b144d7fd82fbb25695b8d0f
TonyVP <tony_vp@hotmail.com>
2022-05-20 02:57:16 +0200

    Thesis delivered

--- GUI_interface/config.py
+++ GUI_interface/config.py
@@ -1,8 +1,14 @@
+"""
+PYTHON SCRIPT FOR GUI APPLICATION FOR ROV. CONTAINS THE GLOBAL VARIABLES
+THAT ARE SHARED BETWEEN MAIN.PY AND INTERFACING.PY, RESPECTIVELY COMMUNICATION
+AND MAIN GUI SCRIPTS.
+"""
 import numpy as np
 
+# Holds the frame communicated from Raspberry Pi
 rovCamera = np.array([])
 
-# Commands from GUI to raspberry initial values
+# Commands from GUI to raspberry with initial values
 light = 0
 motorSpeed = 0
 runZone = -1
@@ -11,13 +17,13 @@ mode = 1
 takePhoto = False
 takeVideo = False
 
-# Sensor information from Raspberry
+# Sensor information from Raspberry with inital values
 temp = 0
 pressure = 0
 leak = 0
 angle = 0
 interlockedZones = [True] * 8
 
-# When new command has been selected on GUI, the TCP
-# communication will send to Raspberry
+# Boolean flag that indicates if new actions have been
+# performed on GUI
 newCommands = False 
--- GUI_interface/main.py
+++ GUI_interface/main.py
@@ -1,37 +1,42 @@
-from concurrent.futures import thread
+
+"""
+MAIN PROGRAM FOR GUI APPLICATION. HANDLES THE COMMUNICATION WITH THE 
+RASPBERRY PI INSIDE THE ROV ENCLOSURE. THIS PROGRAM CONTAINS A TCP 
+CLIENT AND A UDP SERVER. PROGRAM IS DEPENDANT ON CONFIG.PY FOR GLOBAL
+VARIABLES, AND INTERFACING.PY FOR GUI APPLICATION FUNCTIONALITY.
+"""
+
+from xmlrpc.client import Server
 import socket
 import pickle
 import cv2
 import time
 import numpy as np
-from ast import Lambda
-from PyQt5 import QtGui
-from PyQt5 import QtWidgets
-from PyQt5 import QtCore
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout
-from PyQt5.QtGui import QPixmap
 import sys
 import cv2
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread,QTime,QTimer
 from math import *
 import threading
 import config
-# Importing classes generated from GUI creation
 from interfacing import VideoThread, App
 import struct
 
-# Sonar constants
-MAX_RANGE = 80*200*1450/2   # Might have to be found from sonar class
+# Sonar constants needed for plotting visualization
+MAX_RANGE = 80*200*1450/2   
 LENGTH = 640
+CENTER = (LENGTH/2,LENGTH/2)
 image = np.zeros((LENGTH, LENGTH, 1), np.uint8)
 
-
+"""
+Function that creates a circular plot of the sonar readings. Takes
+in the angle scanned, step size for next angle and a list of echo
+strengths.
+"""
 def plotSonarInput(angle, step, data_lst):
 
-    center = (LENGTH/2,LENGTH/2)
-    linear_factor = len(data_lst)/center[0]
-    for i in range(int(center[0])):
-        if(i < center[0]*MAX_RANGE/MAX_RANGE):
+    linear_factor = len(data_lst)/CENTER[0]
+    for i in range(int(CENTER[0])):
+        if(i < CENTER[0]*MAX_RANGE/MAX_RANGE):
             try:
                 pointColor = data_lst[int(i*linear_factor-1)]
             except IndexError:
@@ -39,94 +44,114 @@ def plotSonarInput(angle, step, data_lst):
         else:
             pointColor = 0
         for k in np.linspace(0,step,8*step):
-            image[int(center[0]+i*cos(2*pi*(angle+k)/400)), int(center[1]+i*sin(2*pi*(angle+k)/400)), 0] = pointColor
+            image[int(CENTER[0]+i*cos(2*pi*(angle+k)/400)), int(CENTER[1]+i*sin(2*pi*(angle+k)/400)), 0] = pointColor
 
     # Updates GUI animation with new sonar plot 
     a.update_sonar(cv2.applyColorMap(image, cv2.COLORMAP_JET))    
 
+"""
+Function that handles the TCP Communication between the GUI and the
+Raspberry Pi. 
+"""
 def TCPCom():
+    # Define connection parameters of RPi server
     SERVER = "169.254.226.72"
     PORT = 1422
     HEADERSIZE = 10
 
+    # Using socket library, initialize a communication object
+    # and initialize TCP connection.
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((SERVER, PORT))
+
     print(f"[NEW CONNECTION] With {SERVER} established")
 
+    
+    full_msg = b''
+    new_msg = True
     while 1:
-        full_msg = b''
-        new_msg = True
-        while 1:
-            # print("Receiving data from raspberry")
-            msg = s.recv(8192) # Prev 16
-            if new_msg:
-                msglen = int(msg[:HEADERSIZE])
-                new_msg = False
+        # Receives TCP packets and stores in variable
+        msg = s.recv(8192) # 8192
 
-            full_msg += msg
-            # print(f"Full messsage: {len(full_msg)}")
+        # If the start of a data message is sent, find length of expected information
+        if new_msg:
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
 
-            if len(full_msg)-HEADERSIZE == msglen:
+        # Sum all packets to a full message
+        full_msg += msg
 
-                RaspDataIn = pickle.loads(full_msg[HEADERSIZE:])
+        # If length of message is as previously decided, unpack data and call functions
+        if len(full_msg)-HEADERSIZE == msglen:
 
-                a.setDisplayValues(RaspDataIn["temp"], RaspDataIn["depth"], RaspDataIn["leak"],
-                RaspDataIn["lockedZones"], RaspDataIn["salinity"], RaspDataIn["conductivity"],
-                RaspDataIn["density"])
-                plotSonarInput(RaspDataIn["angle"], RaspDataIn["step"], RaspDataIn["dataArray"])
+            RaspDataIn = pickle.loads(full_msg[HEADERSIZE:])
+
+            a.setDisplayValues(RaspDataIn["temp"], RaspDataIn["depth"], RaspDataIn["leak"],
+            RaspDataIn["lockedZones"], RaspDataIn["salinity"], RaspDataIn["conductivity"],
+            RaspDataIn["density"])
+            plotSonarInput(RaspDataIn["angle"], RaspDataIn["step"], RaspDataIn["dataArray"])
                 
-                new_msg = True
-                full_msg = b""
+            # Resets for next message
+            new_msg = True
+            full_msg = b""
 
-            # If TCP packet was not succesfully unpacked last message
-            # reset to input buffer
-            if len(full_msg) > 2500:
-                new_msg = True
-                full_msg = b""
+        # If TCP packet was not succesfully unpacked last message
+        # reset the input buffer
+        if len(full_msg) > 2500:    
+            new_msg = True
+            full_msg = b""
                 
                 
-            # If GUI input variables have been changed, respond with new commands
-            if config.newCommands:
-                print("[ATTENTION] New commands sent to Raspberry")
-                config.newCommands = False
+        # If user has performed actions on GUI, sends commands over TCP to RPi
+        if config.newCommands:
+            print("[ATTENTION] New commands sent to Raspberry")
+            config.newCommands = False
 
-                RaspDataOut = {
-                    "light": config.light,
-                    "motorSpeed": config.motorSpeed,
-                    "runZone": config.runZone,
-                    "forceReset": config.forceReset,
-                    "mode": config.mode,
-                    "takePhoto": config.takePhoto,
-                    "takeVideo": config.takeVideo
-                }
+            RaspDataOut = {
+                "light": config.light,
+                "motorSpeed": config.motorSpeed,
+                "runZone": config.runZone,
+                "forceReset": config.forceReset,
+                "mode": config.mode,
+                "takePhoto": config.takePhoto,
+                "takeVideo": config.takeVideo
+            }
 
-                print(RaspDataOut)  # Data that was sent through TCP
-                RaspDataOut = pickle.dumps(RaspDataOut)
-                RaspDataOut = bytes(f'{len(RaspDataOut):<{HEADERSIZE}}', 'utf-8') + RaspDataOut
-                s.send(RaspDataOut)
-            
+            RaspDataOut = pickle.dumps(RaspDataOut)
+            RaspDataOut = bytes(f'{len(RaspDataOut):<{HEADERSIZE}}', 'utf-8') + RaspDataOut
+            s.send(RaspDataOut)
+
+"""
+Function that handles the UDP communication between the GUI and the 
+Raspberry Pi.
+"""        
 def UDPCom():
+    # Datagram set to maximum allowable size
     MAX_DGRAM = 2**16
+    dat = b''
 
+    """
+    Function that dumps the UDP buffer.
+    """
     def dump_buffer(s):
-    # Emptying buffer frame
         while True:
             seg, addr = s.recvfrom(MAX_DGRAM)
             print(seg[0])
             if struct.unpack('B', seg[0:1])[0] == 1:
-                print("finish emptying buffer")
+                print("UDP input buffer emptied")
             break
-
-    # Getting image udp frame & concate before decode and output image
         
-    # Set up UDP socket server
+    # Using socket library, initialize a communication object
+    # and initialize UDP connection.
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(('169.254.226.73', 20001))
 
-    dat = b''
+    # Dumps buffer, so that completely new data will be read at this point 
     dump_buffer(s)
 
-    while True:
+    # Continuously checking for UDP datagrams and unpacks if an entire message
+    # has been received, else summing up datagrams until this is true
+    while 1:
         seg, _ = s.recvfrom(MAX_DGRAM)
         if struct.unpack("B", seg[0:1])[0] > 1:
             dat += seg[1:]
@@ -137,15 +162,12 @@ def UDPCom():
                 config.rovCamera = img
             except:
                 print("error (-215:Assertion failed)")
-            if cv2.waitKey(20) & 0xFF == ord('q'):  # Changed from waitkey(1)
-                print("Waitkey event entered!")
+            if cv2.waitKey(20) & 0xFF == ord('q'):  
                 break
             dat = b''
 
     cv2.destroyAllWindows()
     s.close()
-
-
 
 
 if __name__=="__main__":
